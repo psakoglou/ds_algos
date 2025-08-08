9724389a53fe581be358e0eec66c9d4408807f09
Pavlos Sakoglou <psakoglou@gmail.com>
2025-08-08 13:36:18 -0400

    insert at tail

--- ds/double_linked_list.cpp
+++ ds/double_linked_list.cpp
@@ -13,6 +13,18 @@ private:
 
 public:
     DLL();
+
+    void push_back(Node *n)
+    {
+        if (head == nullptr)
+        {
+            head = n;
+            tail = head;
+            return;
+        }
+        tail->next = n;
+        n = tail;
+    }
 };
 
 int main()
